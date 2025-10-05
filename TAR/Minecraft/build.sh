1d0c0136b58eb494e581ba53500fa18503f9ecb2
BakedPotato <90150681+bpotato198@users.noreply.github.com>
2025-10-05 10:01:38 +0200

    Add files via upload

---
+++ TAR/Minecraft/build.sh
@@ -0,0 +1,20 @@
+curl -o Minecraft.tar.gz https://launcher.mojang.com/download/Minecraft.tar.gz  && tar -xpvf Minecraft.tar.gz && cd minecraft-launcher && touch minecraft.desktop &&
+cat <<EOF > "minecraft.desktop"
+[Desktop Entry]
+Type=Application
+Version=1.0
+Name=Minecraft Launcher
+Comment=Official Minecraft Launcher
+Path=/usr/games/minecraft
+Exec=minecraft-launcher
+Terminal=false
+Categories=Game;
+EOF
+
+sudo mkdir /usr/games/minecraft && sudo cp minecraft-launcher /usr/games/minecraft && sudo cp minecraft.desktop ~/.local/share/applications/ && chmod +x ~/.local/share/applications/minecraft.desktop
+
+
+
+
+
+
